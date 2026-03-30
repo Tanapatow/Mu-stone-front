622c854245df620e44f057ef6b8f50ac83149dd4
Tanapatow <tanapat.ow@gmail.com>
2026-03-30 15:15:30 +0700

    before merge pChun

--- src/app/(main)/fortune/predict/page.tsx
+++ src/app/(main)/fortune/predict/page.tsx
@@ -2,17 +2,31 @@ import PredictedResult from '@/components/features/fortune/predicted-result';
 import ProductRecommendSection from '@/components/features/fortune/product-recommend-section';
 import Image from 'next/image';
 
-export default function PredictPage() {
+const simLoading = (second: number = 3) =>
+  new Promise((resolve) => setTimeout(() => resolve(null), second * 1000));
+
+export default async function PredictPage() {
+  // await simLoading(5);
   return (
     <div className="relative min-h-screen">
       <Image
         src="/hero-bg.png"
         alt="bg"
         fill
+        sizes="80px"
         className="object-cover -z-10"
         priority
       />
       <PredictedResult />
+      <div className="flex justify-center items-center">
+        <div className="animate-bounce">
+          <div
+            className="w-10 h-10 border-r-4 border-b-4 border-white rotate-45 
+                shadow-[0_0_10px_rgba(0,0,0,0.5)]
+                animate-pulse"
+          ></div>
+        </div>
+      </div>
       <ProductRecommendSection />
       <div className="flex flex-col items-center">
         <div className="max-w-2/4 text-center">
--- src/components/features/fortune/predicted-result.tsx
+++ src/components/features/fortune/predicted-result.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image';
+import CardFlip from './card-flip';
 
 export default function PredictedResult() {
   return (
     <div className="mt-16 py-16 px-16 bg-black/60">
       <div className="flex flex-col gap-8">
         <div className="flex gap-8">
-          <Image src="/tarotcards/A1.png" alt="card" width={120} height={180} />
+          <CardFlip imageUrl="/tarotcards/A1.png" />
           <div className="flex flex-col gap-2 text-white font-aclonica">
             <h1 className="text-2xl">1. ตัวคุณเป็นเช่นไรในช่วงนี้</h1>
             <h2 className="text-lg">ไพ่ 3 ดาบ (THE THREE OF SWORDS)</h2>
@@ -26,10 +26,12 @@ export default function PredictedResult() {
           </div>
         </div>
         <div className="flex gap-8">
-          <Image src="/tarotcards/C5.png" alt="card" width={120} height={180} />
+          <CardFlip imageUrl="/tarotcards/C14.png" />
           <div className="flex flex-col gap-2 text-white font-aclonica">
-            <h1 className="text-2xl">1. ตัวคุณเป็นเช่นไรในช่วงนี้</h1>
-            <h2 className="text-lg">ไพ่ 3 ดาบ (THE THREE OF SWORDS)</h2>
+            <h1 className="text-2xl">
+              2. สิ่งที่ผ่านมาในอดีต (ลูกน้อง/บริวาร)
+            </h1>
+            <h2 className="text-lg">เทพีแห่งพรหมลิขิต (WHEEL OF FORTUNE)</h2>
             <p className="text-sm">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil et
               quos odit enim, velit quis sequi quae nostrum. Fugiat deleniti
@@ -47,15 +49,10 @@ export default function PredictedResult() {
           </div>
         </div>
         <div className="flex gap-8">
-          <Image
-            src="/tarotcards/WA11.png"
-            alt="card"
-            width={120}
-            height={180}
-          />
+          <CardFlip imageUrl="/tarotcards/SW7.png" />
           <div className="flex flex-col gap-2 text-white font-aclonica">
-            <h1 className="text-2xl">1. ตัวคุณเป็นเช่นไรในช่วงนี้</h1>
-            <h2 className="text-lg">ไพ่ 3 ดาบ (THE THREE OF SWORDS)</h2>
+            <h1 className="text-2xl">3.สิ่งที่จะเกิดขึ้นในอนาคต</h1>
+            <h2 className="text-lg">ไพ่ 5 ไม้เท้า (THE FIVES OF WANDS)</h2>
             <p className="text-sm">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil et
               quos odit enim, velit quis sequi quae nostrum. Fugiat deleniti
